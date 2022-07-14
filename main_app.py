ffd50e8a2182e5a9e466efc64efdd712ebcf4f42
TerezaSe <104388527+TerezaSe@users.noreply.github.com>
2022-07-14 20:06:54 +0200

    dopolední a odpolední stanice grafy

--- main_app.py
+++ main_app.py
@@ -11,16 +11,26 @@ import plotly.express as px
 #data
 ##################
 engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
-query = """SELECT
+query_morning = """SELECT
             start_station_latitude as lat,
             start_station_longitude as lon
         FROM edinburgh_bikes
-        LIMIT 20000
+            WHERE hour(started_at) BETWEEN 6 and 9
+        LIMIT 100000
 
 """
 
-df_bikes = pd.read_sql(sql=query, con=engine)
+query_afternoon = """SELECT
+            start_station_latitude as lat,
+            start_station_longitude as lon
+        FROM edinburgh_bikes
+            WHERE hour(started_at) BETWEEN 15 and 19
+        LIMIT 100000
 
+"""
+
+df_bikes_morning = pd.read_sql(sql=query_morning, con=engine)
+df_bikes_afternoon = pd.read_sql(sql=query_afternoon, con=engine)
 
 ##################
 #vizualizace
@@ -33,9 +43,12 @@ st.write('Toto je moje prvni aplikace, kterou delam')
 
 page = st.sidebar.radio('Select page',['Mapa','Thompson'])
 if page == 'Mapa':
-    st.write('Mapa používání sdílených dat v Edinbourgu')
-    st.map(df_bikes)
-    
+    st.header('Mapa používání sdílených dat v Edinbourgu')
+    st.write('Počáteční stanice ráno mezi 6 a 9')
+    st.map(df_bikes_morning)
+    st.write('Počáteční stanice odpoledne mezi 15 a 19')
+    st.map(df_bikes_afternoon)
+
 
 if page == 'Thompson':
     st.write('Thompson sampling')
