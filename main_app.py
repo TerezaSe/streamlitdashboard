81c0c9b29b031814e8e39061263cb0a779f904f0
TerezaSe <104388527+TerezaSe@users.noreply.github.com>
2022-07-14 19:30:22 +0200

    dodělání mapy
    místo tetsu je tam mapa

--- main_app.py
+++ main_app.py
@@ -1,11 +1,34 @@
 import streamlit as st
+from sqlalchemy import create_engine
+import pandas as pd
+import pymysql
+import plotly.graph_objects as go
+import plotly.express as px
+
+
+
+##################
+#data
+##################
+engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
+df_bikes = pd.read_sql(sql='select * from edinburgh_bikes', con=engine)
+
+
+##################
+#vizualizace
+##################
+
 
 st.title('Moje prvni appka')
 
 st.write('Toto je moje prvni aplikace, kterou delam')
 
 page = st.sidebar.radio('Select page',['Test','Thompson'])
-if page == 'Test':
-    st.write('Toto je moje prvni aplikace, kterou delam')
+if page == 'Mapa':
+    st.write('Mapa používání sdílených dat v Edinbourgu')
+    fig = px.scatter_mapbox(df_temp,lat='start_station_latitude', lon='start_station_longitude')
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.show()
+
 if page == 'Thompson':
     st.write('Thompson sampling')
