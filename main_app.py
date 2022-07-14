709e2a7656bf880f1ca6bfb954ab08d739b84794
TerezaSe <104388527+TerezaSe@users.noreply.github.com>
2022-07-14 19:17:16 +0200

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -3,3 +3,10 @@ import streamlit as st
 st.title('Moje prvni appka')
 
 st.write('Toto je moje prvni aplikace, kterou delam')
+
+page = st.sidebar.radio('Select page',['Test','Thomson'])
+if pafe == 'Test':
+    st.write('Toto je moje prvni aplikace, kterou delam')
+
+if page == 'Thompson':
+    st.write('Thompson sampling')
