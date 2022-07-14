83a05bcc0de6d362ffaf253aeb88b98f3acb9bdc
TerezaSe <104388527+TerezaSe@users.noreply.github.com>
2022-07-14 19:18:16 +0200

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -5,7 +5,7 @@ st.title('Moje prvni appka')
 st.write('Toto je moje prvni aplikace, kterou delam')
 
 page = st.sidebar.radio('Select page',['Test','Thomson'])
-if pafe == 'Test':
+if page == 'Test':
     st.write('Toto je moje prvni aplikace, kterou delam')
 
 if page == 'Thompson':
