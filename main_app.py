d520732a5028d7899d79ce6bc1aad3157d4456cf
TerezaSe <104388527+TerezaSe@users.noreply.github.com>
2022-07-14 19:52:13 +0200

    změna mapy

--- main_app.py
+++ main_app.py
@@ -11,7 +11,15 @@ import plotly.express as px
 #data
 ##################
 engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
-df_bikes = pd.read_sql(sql='select * from edinburgh_bikes limit 20000 ', con=engine)
+query = """SELECT
+            start_station_latitude as lat,
+            start_station_longitude as lon
+        FROM edinburgh_bikes
+        LIMIT 20000
+
+"""
+
+df_bikes = pd.read_sql(sql=query, con=engine)
 
 
 ##################
@@ -26,9 +34,8 @@ st.write('Toto je moje prvni aplikace, kterou delam')
 page = st.sidebar.radio('Select page',['Mapa','Thompson'])
 if page == 'Mapa':
     st.write('Mapa používání sdílených dat v Edinbourgu')
-    fig = px.scatter_mapbox(df_temp,lat='start_station_latitude', lon='start_station_longitude')
-    fig.update_layout(mapbox_style="open-street-map")
-    fig.show()
+    st.map(df_bikes)
+    
 
 if page == 'Thompson':
     st.write('Thompson sampling')
